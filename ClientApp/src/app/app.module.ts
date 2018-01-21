668f26ee5e390178f2b2baac1c75d98a5324df18
damienbod <damien_bod@hotmail.com>
2018-01-21 10:32:36 +0100

    adding an angular google oidc client using the dotnet template

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,94 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+
+import {
+  AuthModule,
+  OidcSecurityService,
+  OpenIDImplicitFlowConfiguration,
+  OidcConfigService,
+  AuthWellKnownEndpoints
+} from 'angular-auth-oidc-client';
+import { AutoLoginComponent } from './auto-login/auto-login.component';
+import { routing } from './app.routes';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { AuthorizationGuard } from './authorization.guard';
+
+export function loadConfig(oidcConfigService: OidcConfigService) {
+    console.log('APP_INITIALIZER STARTING');
+    return () => oidcConfigService.load_using_stsServer('https://accounts.google.com');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    AutoLoginComponent,
+    ForbiddenComponent,
+    UnauthorizedComponent,
+    ProtectedComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    AuthModule.forRoot(),
+    FormsModule,
+    routing,
+  ],
+  providers: [
+	  OidcSecurityService,
+	  OidcConfigService,
+	  {
+		  provide: APP_INITIALIZER,
+		  useFactory: loadConfig,
+		  deps: [OidcConfigService],
+		  multi: true
+    },
+    AuthorizationGuard
+	],
+  bootstrap: [AppComponent]
+})
+
+
+export class AppModule {
+    constructor(
+        private oidcSecurityService: OidcSecurityService,
+        private oidcConfigService: OidcConfigService,
+    ) {
+        this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+
+        let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        openIDImplicitFlowConfiguration.stsServer = 'https://accounts.google.com';
+        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44366';
+        openIDImplicitFlowConfiguration.client_id = '188968487735-b1hh7k87nkkh6vv84548sinju2kpr7gn.apps.googleusercontent.com';
+        openIDImplicitFlowConfiguration.response_type = 'id_token token';
+        openIDImplicitFlowConfiguration.scope = 'openid email profile';
+        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
+        openIDImplicitFlowConfiguration.post_login_route = '/home';
+        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+        openIDImplicitFlowConfiguration.trigger_authorization_result_event = true;
+        openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        openIDImplicitFlowConfiguration.log_console_debug_active = true;
+        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 20;
+
+        const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+        authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+
+        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+
+        });
+
+        console.log('APP STARTING');
+    }
+}
+
