443e839295855f770ce1d1b125ec44c71e3c0870
damienbod <damien_bod@hotmail.com>
2018-05-04 20:11:32 +0200

    Updating to angular 6

--- dotnet-angular-google-oidc/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ dotnet-angular-google-oidc/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
