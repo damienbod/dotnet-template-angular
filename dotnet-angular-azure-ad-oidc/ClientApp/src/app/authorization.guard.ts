cee30a7d1d0050bde2fc5ba6549623c8a454cb48
damienbod <damien_bod@hotmail.com>
2018-10-07 20:56:29 +0200

    updating packages

--- dotnet-angular-azure-ad-oidc/ClientApp/src/app/authorization.guard.ts
+++ dotnet-angular-azure-ad-oidc/ClientApp/src/app/authorization.guard.ts
@@ -1,36 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, Route } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Injectable()
-export class AuthorizationGuard implements CanActivate {
+export class AuthorizationGuard implements CanActivate, CanLoad {
+  constructor(private router: Router, private oidcSecurityService: OidcSecurityService) { }
 
-  constructor(
-    private router: Router,
-    private oidcSecurityService: OidcSecurityService
-  ) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.checkUser();
+  }
 
-  public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    console.log(route + '' + state);
+  canLoad(state: Route): Observable<boolean> {
+    return this.checkUser();
+  }
+
+  private checkUser(): any {
     console.log('AuthorizationGuard, canActivate');
 
     return this.oidcSecurityService.getIsAuthorized().pipe(
-      map((isAuthorized: boolean) => {
+      tap((isAuthorized: boolean) => {
         console.log('AuthorizationGuard, canActivate isAuthorized: ' + isAuthorized);
 
-        if (isAuthorized) {
-          return true;
+        if (!isAuthorized) {
+          this.router.navigate(['/unauthorized']);
         }
-
-        this.router.navigate(['/unauthorized']);
-        //if (!window.location.hash) {
-        //  console.log('AuthorizationGuard auto login');
-        //  this.router.navigate(['/autologin']);
-        //}
-
-        return false;
       })
     );
   }
