63aa63a02d9e4b69189776f01bbc945bb7292a74
damienbod <damien_bod@hotmail.com>
2019-06-15 10:45:25 +0200

    updating configuration for the auth module

--- dotnet-angular-azure-ad-oidc/ClientApp/src/app/app.module.ts
+++ dotnet-angular-azure-ad-oidc/ClientApp/src/app/app.module.ts
@@ -9,12 +9,13 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 
 import {
-  AuthModule,
-  OidcSecurityService,
-  OpenIDImplicitFlowConfiguration,
-  OidcConfigService,
-  AuthWellKnownEndpoints
+    AuthModule,
+    OidcSecurityService,
+    ConfigResult,
+    OidcConfigService,
+    OpenIdConfiguration
 } from 'angular-auth-oidc-client';
+
 import { AutoLoginComponent } from './auto-login/auto-login.component';
 import { routing } from './app.routes';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
@@ -70,37 +71,67 @@ export class AppModule {
     private oidcSecurityService: OidcSecurityService,
     private oidcConfigService: OidcConfigService,
   ) {
-    this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+    this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
 
-      const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-      authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+      const config: OpenIdConfiguration = {
+        stsServer: 'https://login.microsoftonline.com/ed48a4e2-9762-4f41-8a15-9bcd99965fab/v2.0/',
+        redirect_url: configResult.customConfig.redirect_url,
+        client_id: configResult.customConfig.client_id,
+        response_type: configResult.customConfig.response_type,
+        scope: configResult.customConfig.scope,
+        post_logout_redirect_uri: configResult.customConfig.post_logout_redirect_uri,
+        start_checksession: configResult.customConfig.start_checksession,
+        silent_renew: configResult.customConfig.silent_renew,
+        silent_renew_url: 'https://localhost:44311/silent-renew.html',
+        post_login_route: '/home',
+        forbidden_route: configResult.customConfig.forbidden_route,
+        unauthorized_route: configResult.customConfig.unauthorized_route,
+        log_console_warning_active: configResult.customConfig.log_console_warning_active,
+        log_console_debug_active: configResult.customConfig.log_console_debug_active,
+        max_id_token_iat_offset_allowed_in_seconds: configResult.customConfig.max_id_token_iat_offset_allowed_in_seconds,
+        history_cleanup_off: true,
+        iss_validation_off: true
+        // disable_iat_offset_validation: true
+      };
 
-      const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-      openIDImplicitFlowConfiguration.stsServer = 'https://login.microsoftonline.com/ed48a4e2-9762-4f41-8a15-9bcd99965fab/v2.0/';//this.oidcConfigService.wellKnownEndpoints.issuer;
-      openIDImplicitFlowConfiguration.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
-      openIDImplicitFlowConfiguration.client_id = this.oidcConfigService.clientConfiguration.client_id;
-      openIDImplicitFlowConfiguration.response_type = this.oidcConfigService.clientConfiguration.response_type;
-      openIDImplicitFlowConfiguration.scope = this.oidcConfigService.clientConfiguration.scope;
-      openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
-      openIDImplicitFlowConfiguration.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
-      openIDImplicitFlowConfiguration.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
-      openIDImplicitFlowConfiguration.silent_renew_url = this.oidcConfigService.clientConfiguration.silent_renew_url;
-      openIDImplicitFlowConfiguration.post_login_route = '/home';
-      openIDImplicitFlowConfiguration.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
-      openIDImplicitFlowConfiguration.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
-      openIDImplicitFlowConfiguration.auto_userinfo = this.oidcConfigService.clientConfiguration.auto_userinfo;
-      openIDImplicitFlowConfiguration.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
-      openIDImplicitFlowConfiguration.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
-      openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds =
-        this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
 
-      openIDImplicitFlowConfiguration.iss_validation_off = true;
+      this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
 
-      this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
-
-      //this.oidcSecurityService.setCustomRequestParameters(this.oidcConfigService.clientConfiguration.additional_login_parameters);
+      this.oidcSecurityService.setCustomRequestParameters(configResult.customConfig.additional_login_parameters);
       this.oidcSecurityService.setCustomRequestParameters({ response_mode: 'fragment' } );
     });
+
+    //this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+
+    //  const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+    //  authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+
+    //  const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+    //  openIDImplicitFlowConfiguration.stsServer = 'https://login.microsoftonline.com/ed48a4e2-9762-4f41-8a15-9bcd99965fab/v2.0/';//this.oidcConfigService.wellKnownEndpoints.issuer;
+    //  openIDImplicitFlowConfiguration.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
+    //  openIDImplicitFlowConfiguration.client_id = this.oidcConfigService.clientConfiguration.client_id;
+    //  openIDImplicitFlowConfiguration.response_type = this.oidcConfigService.clientConfiguration.response_type;
+    //  openIDImplicitFlowConfiguration.scope = this.oidcConfigService.clientConfiguration.scope;
+    //  openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
+    //  openIDImplicitFlowConfiguration.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
+    //  openIDImplicitFlowConfiguration.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
+    //  openIDImplicitFlowConfiguration.silent_renew_url = this.oidcConfigService.clientConfiguration.silent_renew_url;
+    //  openIDImplicitFlowConfiguration.post_login_route = '/home';
+    //  openIDImplicitFlowConfiguration.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
+    //  openIDImplicitFlowConfiguration.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
+    //  openIDImplicitFlowConfiguration.auto_userinfo = this.oidcConfigService.clientConfiguration.auto_userinfo;
+    //  openIDImplicitFlowConfiguration.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
+    //  openIDImplicitFlowConfiguration.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
+    //  openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds =
+    //    this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
+
+    //  openIDImplicitFlowConfiguration.iss_validation_off = true;
+
+    //  this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+
+    //  //this.oidcSecurityService.setCustomRequestParameters(this.oidcConfigService.clientConfiguration.additional_login_parameters);
+    //  this.oidcSecurityService.setCustomRequestParameters({ response_mode: 'fragment' } );
+    //});
 
     console.log('APP STARTING');
   }
--- dotnet-angular-google-oidc/ClientApp/src/app/app.module.ts
+++ dotnet-angular-google-oidc/ClientApp/src/app/app.module.ts
@@ -9,12 +9,13 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 
 import {
-  AuthModule,
-  OidcSecurityService,
-  OpenIDImplicitFlowConfiguration,
-  OidcConfigService,
-  AuthWellKnownEndpoints
+    AuthModule,
+    OidcSecurityService,
+    ConfigResult,
+    OidcConfigService,
+    OpenIdConfiguration
 } from 'angular-auth-oidc-client';
+
 import { AutoLoginComponent } from './auto-login/auto-login.component';
 import { routing } from './app.routes';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
@@ -64,29 +65,56 @@ export class AppModule {
         private oidcSecurityService: OidcSecurityService,
         private oidcConfigService: OidcConfigService,
     ) {
-        this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+      this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
 
-        let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-        openIDImplicitFlowConfiguration.stsServer = 'https://accounts.google.com';
-        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44388';
-        openIDImplicitFlowConfiguration.client_id = '188968487735-b1hh7k87nkkh6vv84548sinju2kpr7gn.apps.googleusercontent.com';
-        openIDImplicitFlowConfiguration.response_type = 'id_token token';
-        openIDImplicitFlowConfiguration.scope = 'openid email profile';
-        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44388/unauthorized';
-        openIDImplicitFlowConfiguration.post_login_route = '/home';
-        openIDImplicitFlowConfiguration.forbidden_route = '/forbidden';
-        openIDImplicitFlowConfiguration.unauthorized_route = '/unauthorized';
-        openIDImplicitFlowConfiguration.trigger_authorization_result_event = true;
-        openIDImplicitFlowConfiguration.log_console_warning_active = true;
-        openIDImplicitFlowConfiguration.log_console_debug_active = true;
-        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 20;
+        const config: OpenIdConfiguration = {
+          stsServer: configResult.customConfig.stsServer,
+          redirect_url: configResult.customConfig.redirect_url,
+          client_id: configResult.customConfig.client_id,
+          response_type: configResult.customConfig.response_type,
+          scope: configResult.customConfig.scope,
+          post_logout_redirect_uri: configResult.customConfig.post_logout_redirect_uri,
+          start_checksession: configResult.customConfig.start_checksession,
+          silent_renew: configResult.customConfig.silent_renew,
+          silent_renew_url: 'https://localhost:44311/silent-renew.html',
+          post_login_route: configResult.customConfig.startup_route,
+          forbidden_route: configResult.customConfig.forbidden_route,
+          unauthorized_route: configResult.customConfig.unauthorized_route,
+          log_console_warning_active: configResult.customConfig.log_console_warning_active,
+          log_console_debug_active: configResult.customConfig.log_console_debug_active,
+          max_id_token_iat_offset_allowed_in_seconds: configResult.customConfig.max_id_token_iat_offset_allowed_in_seconds,
+          history_cleanup_off: true
+          // iss_validation_off: false
+          // disable_iat_offset_validation: true
+        };
 
-        const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-        authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+        this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
+      });
 
-        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
 
-        });
+        //this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+
+        //let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        //openIDImplicitFlowConfiguration.stsServer = 'https://accounts.google.com';
+        //openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44388';
+        //openIDImplicitFlowConfiguration.client_id = '188968487735-b1hh7k87nkkh6vv84548sinju2kpr7gn.apps.googleusercontent.com';
+        //openIDImplicitFlowConfiguration.response_type = 'id_token token';
+        //openIDImplicitFlowConfiguration.scope = 'openid email profile';
+        //openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44388/unauthorized';
+        //openIDImplicitFlowConfiguration.post_login_route = '/home';
+        //openIDImplicitFlowConfiguration.forbidden_route = '/forbidden';
+        //openIDImplicitFlowConfiguration.unauthorized_route = '/unauthorized';
+        //openIDImplicitFlowConfiguration.trigger_authorization_result_event = true;
+        //openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        //openIDImplicitFlowConfiguration.log_console_debug_active = true;
+        //openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 20;
+
+        //const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+        //authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+
+        //this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+
+        //});
 
         console.log('APP STARTING');
     }
